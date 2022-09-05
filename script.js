875e0379aaeb4e39dd6b3845a581b45443ad282f
Umida Boltaeva <umidakhon.boltaeva@gmail.com>
2022-09-05 21:32:00 +1000

    create cards and display book details

--- script.js
+++ script.js
@@ -17,12 +17,53 @@ function addBookToLibrary(book) {
 }
 
 function renderBookList() {
-  const container = document.getElementById('container');
-  myLibrary.forEach((book) => {
-    const node = document.createElement('div');
-    const textnode = document.createTextNode(book.title);
-    node.appendChild(textnode);
-    container.appendChild(node);
+  const cards = document.getElementById('cards');
+  myLibrary.forEach((book, index) => {
+    cards.appendChild(renderBook(book, index));
   });
+}
+
+function renderBook(book, index) {
+  //create parent div.
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  //render title.
+  const title = document.createElement('h3');
+  title.classList.add('title');
+  title.appendChild(document.createTextNode(book.title));
+  card.appendChild(title);
+
+  //render author.
+  const author = document.createElement('h4');
+  author.classList.add('author');
+  author.appendChild(document.createTextNode(book.author));
+  card.appendChild(author);
+
+  //render pages.
+  const pages = document.createElement('h5');
+  pages.classList.add('pages');
+  pages.appendChild(document.createTextNode(book.pages));
+  card.appendChild(pages);
+
+  const statusId = 'status' + index;
+  //render read status label.
+  const readStatus = document.createElement('label');
+  readStatus.classList.add('status');
+  readStatus.setAttribute('for', statusId);
+  readStatus.appendChild(document.createTextNode('Read status:'));
+  card.appendChild(readStatus);
+
+  //render read status checkbox.
+  const checkbox = document.createElement('input');
+  checkbox.id = statusId;
+  checkbox.setAttribute('type', 'checkbox');
+  if (book.read) {
+    checkbox.setAttribute('checked', 'true');
+  }
+  checkbox.appendChild(document.createTextNode(book.checkbox));
+  card.appendChild(checkbox);
+
+  return card;
 }
 renderBookList();
